f139fee6c9dda60f143f876874231dd813b39b37
ljy2855 <ljy2855@naver.com>
2024-05-14 21:11:55 +0900

    finish interrupt handler

---
+++ pra6/interrupt/.interrupt.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/embe2024/Downloads/workspace/pra6/interrupt/interrupt.ko := arm-none-linux-gnueabi-ld -EL -r  -T /work/achroimx_kernel/scripts/module-common.lds --build-id  -o /home/embe2024/Downloads/workspace/pra6/interrupt/interrupt.ko /home/embe2024/Downloads/workspace/pra6/interrupt/interrupt.o /home/embe2024/Downloads/workspace/pra6/interrupt/interrupt.mod.o
---
+++ pra6/interrupt/.tmp_versions/interrupt.mod
@@ -0,0 +1,2 @@
+/home/embe2024/Downloads/workspace/pra6/interrupt/interrupt.ko
+/home/embe2024/Downloads/workspace/pra6/interrupt/interrupt.o
